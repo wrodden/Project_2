06684bb2fa274e95eaa5bafdcbd71295588766db
wrodden <wrodden@iu.edu>
2020-11-30 15:06:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ function drawFirstMap(type) {
 
                     if (parseInt(emp_data[i].id) == parseInt(d.id)) {
                         $('#top-emp').html(emp_data[i].topEmployer);
-                        $('#median-sal').html("Salary Range: $" + emp_data[i].MedianMaxSalary + " - $"+emp_data[i].MedianMinSalary);
+                        $('#median-sal').html("Salary Range: $" + emp_data[i].MedianMinSalary + " - $"+emp_data[i].MedianMaxSalary);
                         $('#job-cnt').html(parseInt(emp_data[i].totalJobCount));
                     }
                     
