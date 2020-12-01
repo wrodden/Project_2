cd9f2e8be833425c5db95505d705f7f8c452faa9
wrodden <wrodden@iu.edu>
2020-11-30 21:52:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -510,7 +510,7 @@ svg.append("text")
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .style("text-anchor", "middle")
-      .text("Unemployment %"); 
+      .text("Percent Unemployed"); 
 	  
 svg.append("text")             
       .attr("transform",
